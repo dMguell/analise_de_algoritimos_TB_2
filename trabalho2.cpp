dadb8eba26f4b1205e8c7d7883dec6e22b5cf997
Mguell <mguellntr@gmail.com>
2024-06-02 14:58:49 -0300

    commit_TB2_Finalizado

--- trabalho2.cpp
+++ trabalho2.cpp
@@ -5,31 +5,74 @@
 #include <string>
 #include <algorithm>
 #include <queue>
+#include <stack>
+#include <climits>
+
 using namespace std;
 
 class Grafo {
 private:
-    unordered_map<char, vector<char>> addList; // um tipo de map que deixa vc somente dizer que tipo de dado vai entrar, tbm deixa vc colocar vetores
+    unordered_map<char, vector<char>> addList1; // um tipo de map que deixa vc somente dizer que tipo de dado vai entrar, tbm deixa vc colocar vetores
                                                                                                                                     // dentro dele
-    unordered_map<char, vector<pair<char, int>>> addList3; // msm coisa, só que o vetor possui 2 campos, vertice destino, e o peso
-    //unordered_map<string, vector<string>> addList_EX2;
+    unordered_map<char, vector<char>> addList2; // msmc coisa que o 1                                                                                                                                   
+    unordered_map<char, vector<pair<char, int>>> addList3; // msm coisa que o 1 e 2, só que o vetor possui 2 campos, vertice destino, e o peso
 
 public:
     // Função para adiciona uma aresta ao grafo
-    void addAresta(char de, char para) {
+    void addAresta1(char de, char para) {
+
         if(de != para){ // se a aresta origem for diferente da destino faça:
-            addList[de].push_back(para); // o "de" está entrando na primeira parte do "unordered_map" (no caso, no primeiro char) enquanto que para 
-                                    // add o char "para" na variavel "vector", é necessario usar o "push_back", além de tbm fazer a conecção do grafo     
+
+            addList1[de].push_back(para); // o "de" está entrando na primeira parte do "unordered_map" (no caso, no primeiro char) enquanto que para 
+                                    // add o char "para" na variavel "vector", é necessario usar o "push_back", além de tbm fazer a conecção do grafo
+            addList1[para].push_back(de); // por se tratar de um grafo não ordenado, é necessário fazer o processo inverso para cada vertice
+
         }else{} // se não, n faça nada
     }                                      
 
     // Printa o grafo
-    void printGrafo() {
-        for (auto it = addList.begin(); it != addList.end(); it++) { // criando uma variavel "it", e percorrendo todas as arestas do grafo
+    void printGrafo1() {
+
+        for (auto it = addList1.begin(); it != addList1.end(); it++) { // criando uma variavel "it", e percorrendo todas as arestas do grafo
+
             cout << it->first << " -> "; // printando o "de cá"
+
             for (char v : it->second) { // criando uma varivel "v" e já tribuiindo o valor de "pra lá" nela, enq tiver uma variavel em "lá", o código roda
+
                 cout << v << " "; // printando o "pra lá"
             }
+
+            cout << endl;
+            cout << endl;
+        }
+    }
+
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+
+    void addAresta2(char de, char para) {
+
+        if(de != para){// se a aresta origem for diferente da destino faça:
+
+            addList2[de].push_back(para); // por se tratar de um grafo orientado, não tem necessidade de fazer os dois caminhos para os vertices
+
+        }else{}
+    }
+
+    // Printa o grafo
+    void printGrafo2() {
+
+        for (auto it = addList2.begin(); it != addList2.end(); it++) { // criando uma variavel "it", e percorrendo todas as arestas do grafo
+
+            cout << it->first << " -> "; // printando o "de cá"
+
+            for (char v : it->second) { // criando uma varivel "v" e já tribuiindo o valor de "pra lá" nela, enq tiver uma variavel em "lá", o código roda
+
+                cout << v << " "; // printando o "pra lá"
+
+            }
+
             cout << endl;
             cout << endl;
         }
@@ -40,16 +83,28 @@ public:
     //-------------------------------------------------------------------------------
 
     void addAresta3(char de, char para, int peso) {
-        addList3[de].push_back({para, peso});
+
+        if(de != para){ // se a aresta origem for diferente da destino faça:
+
+            addList3[de].push_back({para, peso}); // por se tratar de um grafo não ordenado, são feita duas variaveis, uma pra ida e uma para volta
+            addList3[para].push_back({de, peso});
+
+        }else{}
     }
 
     // Imprime o grafo
     void printGrafo3() {
-        for (auto& par : addList3) {
-            cout << par.first << " -> ";
-            for (auto& vizinho : par.second) {
-                cout << "(" << vizinho.first << ", " << vizinho.second << ") ";
+
+        for (auto aaa : addList3) { // for que já diz q aaa é igual ao mapa
+
+            cout << aaa.first << " -> "; // printando o primeiro campo do mapa (no caso o "de cá")
+
+            for (auto vizinho : aaa.second) { // for que já diz q vizinho é igual ao segundo campo de aaa (no caso o "pra lá" e peso)
+
+                cout << "(" << vizinho.first << ", " << vizinho.second << ") "; // pritando separadamento o "pra lá" juntamente com peso da aresta
+
             }
+
             cout << endl;
         }
     }
@@ -58,27 +113,125 @@ public:
     //-------------------------------------------------------------------------------
     //-------------------------------------------------------------------------------
 
-    void bfs(char origem) {
-        unordered_set<char> visitados;
-        queue<char> fila;
+    // função para a busca em largura
+    void buscaL(char inicio) {
 
-        fila.push(origem);
-        visitados.insert(origem);
+        queue<char> q; // fila vazia do c++ para armazenar os vertices que vão ser visitados
+        unordered_set<char> visitado; // msm coisa que o unordered_map, só que tipo um vetor
 
-        while (!fila.empty()) {
-            char verticeAtual = fila.front();
-            fila.pop();
+        q.push(inicio); // variavel diciona o vertice inicial a fila e o marca como visitado
+        visitado.insert(inicio);
 
-            cout << verticeAtual << " ";
+        while (!q.empty()) { // enquanto a fila não estiver vazia faça:
+            
+            char current = q.front(); // pegar o vertice da frente da fila
+            cout << current << " ";
+            q.pop();
 
-            for (const char& vizinho : addList[verticeAtual]) {
-                if (visitados.find(vizinho) == visitados.end()) {
-                    fila.push(vizinho);
-                    visitados.insert(vizinho);
+            //---------------------------------------------------------------------
+
+            // bloco referente aos vizinhos do vertice atual
+            for (char vizinho : addList1[current]) {
+                
+                if (visitado.find(vizinho) == visitado.end()) { // se o vizinho não foi visitado faça:
+                
+                    q.push(vizinho); // marcar o vertice visinho visitado como visitado
+                    visitado.insert(vizinho);
+
+                }
+            }
+        }
+    };
+
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+
+    // função para a busca em profundidade
+    void buscaP(char start, unordered_set<char>& visited) {
+    stack<char> stack; // pilha vazia do c++ para armazenar os vertices que vão ser visitados
+    stack.push(start); // variavel diciona o vertice inicial a fila e o marca como visitado
+
+        while (!stack.empty()) { // enquanto a fila não estiver vazia faça:
+            char current = stack.top();
+            stack.pop();
+
+            if (visited.find(current) == visited.end()) {
+                cout << current << " ";
+                visited.insert(current);
+
+                for (char neighbor : addList2[current]) {
+                    if (visited.find(neighbor) == visited.end()) {
+                        stack.push(neighbor);
+                    }
                 }
             }
         }
     }
+
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+
+    void menorcaminho(char origem, char destino) {
+    unordered_map<char, int> distancia;
+    unordered_map<char, char> anterior;
+    priority_queue<pair<int, char>, vector<pair<int, char>>, greater<pair<int, char>>> pq;
+    
+    for (auto& par : addList3) {
+        distancia[par.first] = INT_MAX;
+        anterior[par.first] = 0;
+    }
+    
+    distancia[origem] = 0;
+    pq.push({0, origem});
+    
+    while (!pq.empty()) {
+        char u = pq.top().second;
+        pq.pop();
+        
+        for (auto& vizinho : addList3[u]) {
+            char v = vizinho.first;
+            int peso = vizinho.second;
+            
+            if (distancia[v] > distancia[u] + peso) {
+                distancia[v] = distancia[u] + peso;
+                anterior[v] = u;
+                pq.push({distancia[v], v});
+            }
+        }
+    }
+    
+    if (distancia[destino] == INT_MAX) {
+        cout << "Não há caminho entre " << origem << " e " << destino << endl;
+    } else {
+        vector<char> caminho;
+        for (char v = destino; v != 0; v = anterior[v]) {
+            caminho.push_back(v);
+        }
+        cout << "Menor caminho entre " << origem << " e " << destino << ": ";
+        for (int i = caminho.size() - 1; i >= 0; i--) {
+            cout << caminho[i];
+            if (i > 0) {
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+        
+        cout << "Arestas:" << endl;
+        for (int i = caminho.size() - 1; i > 0; i--) {
+            char u = caminho[i];
+            char v = caminho[i - 1];
+            for (auto& aresta : addList3[u]) {
+                if (aresta.first == v) {
+                    cout << u << " - " << v << " (" << aresta.second << ")" << endl;
+                    break;
+                }
+            }
+        }
+    }
+}
+
 };
 
 int main() {
@@ -87,44 +240,44 @@ int main() {
 
     //EX: grafo.addAresta('aresta origem', 'aresta destino');
 
+    /*cout << "----------------------------------------------------------------------";
+    cout << endl;
+
     cout << "GRAFO 1";
     cout << endl;
-
-    grafo.addAresta('a', 'b');
-    grafo.addAresta('a', 'e');
-    grafo.addAresta('b', 'f');
-    grafo.addAresta('e', 'f');
-    grafo.addAresta('e', 'i');
-    grafo.addAresta('i', 'f');
-    grafo.addAresta('f', 'c');
-    grafo.addAresta('f', 'g');
-    grafo.addAresta('f', 'j');
-    grafo.addAresta('c', 'g');
-    grafo.addAresta('c', 'h');
-    grafo.addAresta('c', 'd');
-    grafo.addAresta('d', 'h');
-    grafo.addAresta('d', 'g');
-    grafo.addAresta('g', 'h');
-    grafo.addAresta('g', 'l');
-    grafo.addAresta('j', 'k');
-    grafo.addAresta('g', 'k');
-    grafo.addAresta('k', 'l');
-    grafo.addAresta('h', 'l');
-
-    grafo.printGrafo();
-
-    cout << endl;
     cout << endl;
 
-    // Realizando a busca em largura a partir do vértice "a"
-    cout << "Busca em largura a partir do vertice 'b': ";
-    grafo.bfs('b');
+    grafo.addAresta1('a', 'b');
+    grafo.addAresta1('a', 'e');
+    grafo.addAresta1('b', 'f');
+    grafo.addAresta1('e', 'f');
+    grafo.addAresta1('e', 'i');
+    grafo.addAresta1('i', 'f');
+    grafo.addAresta1('f', 'c');
+    grafo.addAresta1('f', 'g');
+    grafo.addAresta1('f', 'j');
+    grafo.addAresta1('c', 'g');
+    grafo.addAresta1('c', 'h');
+    grafo.addAresta1('c', 'd');
+    grafo.addAresta1('d', 'h');
+    grafo.addAresta1('d', 'g');
+    grafo.addAresta1('g', 'h');
+    grafo.addAresta1('g', 'l');
+    grafo.addAresta1('j', 'k');
+    grafo.addAresta1('g', 'k');
+    grafo.addAresta1('k', 'l');
+    grafo.addAresta1('h', 'l');
+
+    grafo.printGrafo1();
+
+    cout << endl;
+
+    cout << "Busca em largura partindo de 'b': ";
+    grafo.buscaL('b'); // altere o vertice inical aq
     cout << endl;
     
     cout << "----------------------------------------------------------------------";
-    cout << endl;
-
-    
+    cout << endl;*/
 
     /*cout << "----------------------------------------------------------------------";
     cout << endl;
@@ -133,22 +286,29 @@ int main() {
     cout << endl;
     cout << endl;
 
-    grafo.addAresta('a', 'b');
-    grafo.addAresta('a', 'e');
-    grafo.addAresta('a', 'f');
-    grafo.addAresta('b', 'e');
-    grafo.addAresta('f', 'e');
-    grafo.addAresta('b', 'c');
-    grafo.addAresta('e', 'g');
-    grafo.addAresta('f', 'h');
-    grafo.addAresta('c', 'd');
-    grafo.addAresta('g', 'h');
-    grafo.addAresta('g', 'c');
-    grafo.addAresta('h', 'd');
+    grafo.addAresta2('a', 'b');
+    grafo.addAresta2('a', 'e');
+    grafo.addAresta2('a', 'f');
+    grafo.addAresta2('b', 'e');
+    grafo.addAresta2('f', 'e');
+    grafo.addAresta2('b', 'c');
+    grafo.addAresta2('e', 'g');
+    grafo.addAresta2('e', 'd');
+    grafo.addAresta2('f', 'h');
+    grafo.addAresta2('c', 'd');
+    grafo.addAresta2('g', 'h');
+    grafo.addAresta2('g', 'c');
+    grafo.addAresta2('h', 'd');
 
-    grafo.printGrafo();*/
+    grafo.printGrafo2();
 
-    /*cout << "----------------------------------------------------------------------";
+    unordered_set<char> visited;
+
+    cout << "Busca em profundidade partindo de 'a': ";
+    grafo.buscaP('a', visited); // altere o vertice inical aq
+    cout << endl;*/
+
+    cout << "----------------------------------------------------------------------";
     cout << endl;
 
     cout << "GRAFO 3";
@@ -170,8 +330,13 @@ int main() {
 
     grafo.printGrafo3();
 
-    cout << "----------------------------------------------------------------------";
-    cout << endl;*/
+    cout << endl;
+    cout << endl;
 
+    grafo.menorcaminho('x', 't');
+    cout << endl;
+
+    cout << "----------------------------------------------------------------------";
+    cout << endl;
 
 }
